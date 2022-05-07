d21c10b5b3921d333c28a5a469a47e1fab74cd60
KD <39858155+khduyentr@users.noreply.github.com>
2022-05-07 09:48:23 +0700

    Update on summarizing pictures - Add a dialog to show choices - Allow user to choose pic from gallery or take new pic

--- app/src/main/java/com/example/usmile/user/fragment/CollectPictureFragment.java
+++ app/src/main/java/com/example/usmile/user/fragment/CollectPictureFragment.java
@@ -1,17 +1,26 @@
 package com.example.usmile.user.fragment;
 
-import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.example.usmile.R;
@@ -24,10 +33,15 @@ public class CollectPictureFragment extends Fragment implements View.OnClickList
     ImageView thirdImageView;
     ImageView fourthImageView;
 
-    final int FIRST_IMAGE = 1;
-    final int SECOND_IMAGE = 2;
-    final int THIRD_IMAGE = 3;
-    final int FOURTH_IMAGE = 4;
+    final int CAPTURE_FIRST_IMAGE = 1;
+    final int CAPTURE_SECOND_IMAGE = 2;
+    final int CAPTURE_THIRD_IMAGE = 3;
+    final int CAPTURE_FOURTH_IMAGE = 4;
+
+    final int LOAD_FIRST_IMAGE = 5;
+    final int LOAD_SECOND_IMAGE = 6;
+    final int LOAD_THIRD_IMAGE = 7;
+    final int LOAD_FOURTH_IMAGE = 8;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -45,41 +59,95 @@ public class CollectPictureFragment extends Fragment implements View.OnClickList
     }
 
 
-    public void openCamera(int requestCode){
+    public void openCamera(int requestCode) {
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, requestCode);
     }
+
+
+    public void openGallery(int requestCode) {
+
+        Intent galleryIntent = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent , requestCode );
+    }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Bitmap bp = (Bitmap) data.getExtras().get("data");
 
-        switch (requestCode) {
-            case FIRST_IMAGE:
-                firstImageView.setImageBitmap(bp);
-                firstImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                break;
-            case SECOND_IMAGE:
-                secondImageView.setImageBitmap(bp);
-                secondImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                break;
-            case THIRD_IMAGE:
-                thirdImageView.setImageBitmap(bp);
-                thirdImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                break;
-            case FOURTH_IMAGE:
-                fourthImageView.setImageBitmap(bp);
-                fourthImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                break;
+        // for capture
+        if (requestCode < 5) {
+            Bitmap bp;
+
+            switch (requestCode) {
+                case CAPTURE_FIRST_IMAGE:
+                    bp = (Bitmap) data.getExtras().get("data");
+                    firstImageView.setImageBitmap(bp);
+                    firstImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    break;
+                case CAPTURE_SECOND_IMAGE:
+                    bp = (Bitmap) data.getExtras().get("data");
+                    secondImageView.setImageBitmap(bp);
+                    secondImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    break;
+                case CAPTURE_THIRD_IMAGE:
+                    bp = (Bitmap) data.getExtras().get("data");
+                    thirdImageView.setImageBitmap(bp);
+                    thirdImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    break;
+                case CAPTURE_FOURTH_IMAGE:
+                    bp = (Bitmap) data.getExtras().get("data");
+                    fourthImageView.setImageBitmap(bp);
+                    fourthImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    break;
+            }
+        }
+        else {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            // Get the cursor
+            Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            // Move to first row
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String imgDecodableString = cursor.getString(columnIndex);
+            cursor.close();
+
+            switch (requestCode) {
+                case LOAD_FIRST_IMAGE:
+                    firstImageView.setImageBitmap(BitmapFactory
+                            .decodeFile(imgDecodableString));
+                    firstImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    break;
+                case LOAD_SECOND_IMAGE:
+                    secondImageView.setImageBitmap(BitmapFactory
+                            .decodeFile(imgDecodableString));
+                    secondImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    break;
+                case CAPTURE_THIRD_IMAGE:
+                    thirdImageView.setImageBitmap(BitmapFactory
+                            .decodeFile(imgDecodableString));
+                    thirdImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    break;
+                case LOAD_FOURTH_IMAGE:
+                    fourthImageView.setImageBitmap(BitmapFactory
+                            .decodeFile(imgDecodableString));
+                    fourthImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    break;
+            }
         }
 
 
 
+
     }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -88,24 +156,88 @@ public class CollectPictureFragment extends Fragment implements View.OnClickList
         return inflater.inflate(R.layout.fragment_collect_picture, container, false);
     }
 
+
+
     @Override
     public void onClick(View view) {
-        int id = view.getId();
 
-        switch (id) {
-            case R.id.firstImageView:
-                openCamera(FIRST_IMAGE);
-                break;
-            case R.id.secondImageView:
-                openCamera(SECOND_IMAGE);
-                break;
-            case R.id.thirdImageView:
-                openCamera(THIRD_IMAGE);
-                break;
-            case R.id.fourthImageView:
-                openCamera(FOURTH_IMAGE);
-                break;
-        }
+        int order = view.getId();
+        openChooseSourceOfPictureDialog(order);
+
+    }
+
+    private void openChooseSourceOfPictureDialog(int order) {
+
+        final Dialog dialog = new Dialog(getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.choose_source_of_picture_layout);
+
+
+        Window window = dialog.getWindow();
+
+        if (window == null)
+            return;
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = Gravity.BOTTOM;
+
+        dialog.setCancelable(true);
+
+        // binding here
+        ImageView cameraImageView = dialog.findViewById(R.id.fromCamImageView);
+        ImageView galleryImageView = dialog.findViewById(R.id.fromGalleryImageView);
+
+        cameraImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (order) {
+                    case R.id.firstImageView:
+                        openCamera(CAPTURE_FIRST_IMAGE);
+                        break;
+                    case R.id.secondImageView:
+                        openCamera(CAPTURE_SECOND_IMAGE);
+                        break;
+                    case R.id.thirdImageView:
+                        openCamera(CAPTURE_THIRD_IMAGE);
+                        break;
+                    case R.id.fourthImageView:
+                        openCamera(CAPTURE_FOURTH_IMAGE);
+                        break;
+                }
+
+                dialog.dismiss();
+            }
+        });
+
+        galleryImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                switch (order) {
+                    case R.id.firstImageView:
+                        openGallery(LOAD_FIRST_IMAGE);
+                        break;
+                    case R.id.secondImageView:
+                        openGallery(LOAD_SECOND_IMAGE);
+                        break;
+                    case R.id.thirdImageView:
+                        openGallery(LOAD_THIRD_IMAGE);
+                        break;
+                    case R.id.fourthImageView:
+                        openGallery(LOAD_FOURTH_IMAGE);
+                        break;
+                }
+
+                dialog.dismiss();
+
+            }
+        });
+
+        dialog.show();
+
     }
 
 }

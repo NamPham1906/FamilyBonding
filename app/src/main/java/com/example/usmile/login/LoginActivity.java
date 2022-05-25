150af914d5d98859003d0fb5f704ab98a8cd5c14
NamPham1906 <61858329+NamPham1906@users.noreply.github.com>
2022-05-25 20:07:54 +0700

    add login fragment

--- app/src/main/java/com/example/usmile/login/LoginActivity.java
+++ app/src/main/java/com/example/usmile/login/LoginActivity.java
@@ -1,47 +1,51 @@
 package com.example.usmile.login;
 
+import static java.lang.Thread.sleep;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.usmile.Account.Account;
 import com.example.usmile.Account.AccountFactory;
 import com.example.usmile.R;
 import com.example.usmile.user.UserMainActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     FragmentManager fragmentManager;
     Fragment fragment = null;
     String accountType = "";
-    Account account;
-
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton =  this.findViewById(R.id.loginBtn);
+        loginButton.setOnClickListener(this);
         fragmentManager = getSupportFragmentManager();
         // fragmentManager.beginTransaction().replace(R.id.mainFragmentHolder, fragment).commit();
 
+    }
 
+
+
+    @Override
+    public void onClick(View v) {
         accountType = AccountFactory.USERSTRING;
 
         Intent intent = new Intent(getApplicationContext(), AccountFactory.createAccountClass(accountType));
-        account = AccountFactory.createAccount(accountType);
+        Account account = AccountFactory.createAccount(accountType);
         intent.putExtra(account.type(), account);
         startActivity(intent);
         this.finish();
-
-
-
-
-    }
-
-    public void startHomeActivity (String type){
-
     }
 }
--- app/src/main/java/com/example/usmile/login/fragment/FogotPasswordFragment.java
+++ app/src/main/java/com/example/usmile/login/fragment/FogotPasswordFragment.java
@@ -9,13 +9,13 @@ import androidx.fragment.app.Fragment;
 
 import com.example.usmile.R;
 
-public class LoginFragment extends Fragment {
+public class FogotPasswordFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_fogot_password, container, false);
     }
 
 }

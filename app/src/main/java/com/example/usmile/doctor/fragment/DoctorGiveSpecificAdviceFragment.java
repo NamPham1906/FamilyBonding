2377e3aa8092c73196fb987410cb54683ecffef1
dieuhuong1211 <huong.bn.vt@gmail.com>
2022-06-22 01:36:40 +0700

    gửi tư vấn

--- app/src/main/java/com/example/usmile/doctor/fragment/DoctorGiveSpecificAdviceFragment.java
+++ app/src/main/java/com/example/usmile/doctor/fragment/DoctorGiveSpecificAdviceFragment.java
@@ -1,11 +1,18 @@
 package com.example.usmile.doctor.fragment;
 
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +21,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.usmile.R;
 import com.example.usmile.doctor.DoctorMainActivity;
+import com.example.usmile.utilities.Constants;
 import com.example.usmile.utilities.PreferenceManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class DoctorGiveSpecificAdviceFragment extends Fragment implements View.OnClickListener{
 
@@ -40,6 +60,11 @@ public class DoctorGiveSpecificAdviceFragment extends Fragment implements View.O
     TextView sendButton;
 
     PreferenceManager preferenceManager;
+
+    AlertDialog cancelDialog;
+    AlertDialog.Builder dialogBuilder;
+
+    Fragment fragment;
 
     DoctorMainActivity main;
     @Override
@@ -74,7 +99,37 @@ public class DoctorGiveSpecificAdviceFragment extends Fragment implements View.O
     }
 
     private void loadHealthRecordDetails() {
+        String healthRecordId = preferenceManager.getString(Constants.KEY_HEALTH_RECORD_ID);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection(Constants.KEY_COLLECTION_HEALTH_RECORD)
+                .whereEqualTo(Constants.KEY_HEALTH_RECORD_ID, healthRecordId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(Task<QuerySnapshot> task) {
+                        DocumentSnapshot doc = task.getResult().getDocuments().get(0);
+
+                        List<String> healthPictures = (ArrayList) doc.get(Constants.KEY_HEALTH_RECORD_PICTURES);
+
+                        encodeImage1 = healthPictures.get(0);
+                        encodeImage2 = healthPictures.get(1);
+                        encodeImage3 = healthPictures.get(2);
+                        encodeImage4 = healthPictures.get(3);
+
+                        firstImageView.setImageBitmap(decodeImage(encodeImage1));
+                        secondImageView.setImageBitmap(decodeImage(encodeImage2));
+                        thirdImageView.setImageBitmap(decodeImage(encodeImage3));
+                        fourthImageView.setImageBitmap(decodeImage(encodeImage4));
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
     @Override
@@ -82,10 +137,127 @@ public class DoctorGiveSpecificAdviceFragment extends Fragment implements View.O
         int id = view.getId();
         switch (id) {
             case R.id.sendButton:
+                if (!isCompleted())
+                    return;
+                try{
+                    createSendAdvicesDialog();
+                }
+                catch (Exception e)
+                {
+                    Log.e("CANCEL DIALOG",e.getMessage());
+                }
                 break;
         }
     }
 
+    private void sendAdvices() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference
+                = db.collection(Constants.KEY_COLLECTION_HEALTH_RECORD)
+                .document(preferenceManager.getString(Constants.KEY_HEALTH_RECORD_ID));
+
+        List<String> advices = new ArrayList<String>();
+        if(!isFillEditText(firstDetailAdvice))
+            advices.add("");
+        else
+            advices.add(firstDetailAdvice.getText().toString().trim());
+
+        if(!isFillEditText(secondDetailAdvice))
+            advices.add("");
+        else
+            advices.add(secondDetailAdvice.getText().toString().trim());
+
+        if(!isFillEditText(thirdDetailAdvice))
+            advices.add("");
+        else
+            advices.add(thirdDetailAdvice.getText().toString().trim());
+
+        if(!isFillEditText(fourthDetailAdvice))
+            advices.add("");
+        else
+            advices.add(fourthDetailAdvice.getText().toString().trim());
+
+        advices.add(doctorRequirementTextView.getText().toString().trim());
+
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_HEALTH_RECORD_ADVICES, advices);
+        updates.put(Constants.KEY_HEALTH_RECORD_ACCEPTED, true);
+
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    showToast("Updated successfully");
+
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("update health record", e.getMessage());
+                });
+    }
+
+    public void createSendAdvicesDialog(){
+        dialogBuilder = new AlertDialog.Builder(main);
+        final View quitPopup = getLayoutInflater().inflate(R.layout.popup_send_advices_health_record, null);
+
+        Button quitBtn = (Button) quitPopup.findViewById(R.id.btnQuit);
+        Button cancelBtn = (Button) quitPopup.findViewById(R.id.btnCancel);
+        dialogBuilder.setView(quitPopup);
+        cancelDialog = dialogBuilder.create();
+        cancelDialog.show();
+        cancelDialog.setCanceledOnTouchOutside(false);
+        cancelDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        quitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //dimiss dialog
+                sendAdvices();
+                cancelDialog.dismiss();
+                fragment = new ReceivedHealthRecordListFragment();
+                openNewFragment(view, fragment);
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //dimiss dialog
+                cancelDialog.dismiss();
+            }
+        });
+    }
+
+    private boolean isFillEditText(EditText editText) {
+        String input = editText.getText().toString().trim();
+
+        if (input.isEmpty()) {
+            editText.setError("Không được để trống");
+            return false;
+        } else {
+            editText.setError(null);
+            return true;
+        }
+    }
+    private boolean isCompleted() {
+        if (!isFillEditText(doctorRequirementTextView))
+            return false;
+        return true;
+    }
+
+    private void openNewFragment(View view, Fragment nextFragment) {
+        AppCompatActivity activity = (AppCompatActivity) view.getContext();
+        activity.getSupportFragmentManager().beginTransaction()
+                .replace(R.id.mainFragmentHolder, nextFragment).addToBackStack(null).commit();
+    }
+
+    private Bitmap decodeImage(String encodedImage) {
+        byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+        return bitmap;
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+    }
 
 
 }

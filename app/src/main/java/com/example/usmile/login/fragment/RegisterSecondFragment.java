f73f7b8212ea0ca9db30f831e0fe7120ae43943b
dieuhuong1211 <huong.bn.vt@gmail.com>
2022-06-25 23:39:34 +0700

    mask d/m/y for date text in sign up

--- app/src/main/java/com/example/usmile/login/fragment/RegisterSecondFragment.java
+++ app/src/main/java/com/example/usmile/login/fragment/RegisterSecondFragment.java
@@ -14,7 +14,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +38,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class RegisterSecondFragment extends Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener {
@@ -107,6 +111,81 @@ public class RegisterSecondFragment extends Fragment implements AdapterView.OnIt
         layoutImage = (FrameLayout) view.findViewById(R.id.layoutImage);
         imageProfile = (RoundedImageView) view.findViewById(R.id.imageProfile);
         textAddImage = (TextView) view.findViewById(R.id.textAddImage);
+
+        dobText.addTextChangedListener(datetimeTextWatcher);
+    }
+
+    private TextWatcher datetimeTextWatcher = new TextWatcher() {
+        private String current = "";
+        private String ddmmyyyy = "________";
+        private Calendar cal = Calendar.getInstance();
+
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        public void afterTextChanged(Editable s) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!s.toString().equals(current)) {
+                String clean = s.toString().replaceAll("[^\\d.]", "");
+                String cleanC = current.replaceAll("[^\\d.]", "");
+
+                int cl = clean.length();
+                int sel = cl;
+                for (int i = 2; i <= cl && i < 6; i += 2) {
+                    sel++;
+                }
+                //Fix for pressing delete next to a forward slash
+                if (clean.equals(cleanC)) sel--;
+
+                if (clean.length() < 8){
+                    clean = clean + ddmmyyyy.substring(clean.length());
+                }else{
+                    //This part makes sure that when we finish entering numbers
+                    //the date is correct, fixing it otherwise
+                    int day  = Integer.parseInt(clean.substring(0,2));
+                    int mon  = Integer.parseInt(clean.substring(2,4));
+                    int year = Integer.parseInt(clean.substring(4,8));
+
+                    if(mon > 12) mon = 12;
+                    cal.set(Calendar.MONTH, mon-1);
+
+                    int todayYear = Calendar.getInstance().get(Calendar.YEAR);
+                    year = (year<1900)?1900:(year>todayYear)?todayYear:year;
+                    cal.set(Calendar.YEAR, year);
+                    // ^ first set year for the line below to work correctly
+                    //with leap years - otherwise, date e.g. 29/02/2012
+                    //would be automatically corrected to 28/02/2012
+
+                    day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                    clean = String.format("%02d%02d%02d",day, mon, year);
+                }
+
+                clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                        clean.substring(2, 4),
+                        clean.substring(4, 8));
+
+                sel = sel < 0 ? 0 : sel;
+                current = clean;
+                Log.d("duplicate?", String.valueOf(start) + " " + String.valueOf(before) +  " "  +  String.valueOf(count));
+                dobText.setText(current);
+                dobText.setSelection(sel < current.length() ? sel : current.length());
+            }
+        }
+    };
+
+    private boolean isFillDoBEditText(EditText editText) {
+        String input = editText.getText().toString().trim();
+        input = input.replaceAll("[^\\d.]", "");
+        Log.d("316", input + " " + String.valueOf(input.length()));
+        if (input.length() < 8) {
+            editText.setError("Chưa đủ ngày/tháng/năm");
+            return false;
+        } else {
+            editText.setError(null);
+            return true;
+        }
     }
 
     private List<String> getGenderList() {
@@ -171,7 +250,7 @@ public class RegisterSecondFragment extends Fragment implements AdapterView.OnIt
 
         if (!isFillEditText(fullNameText))
             return false;
-        if (!isFillEditText(dobText))
+        if (!isFillDoBEditText(dobText))
             return false;
         if (!isFillEditText(phoneText))
             return false;
